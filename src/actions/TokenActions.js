43410c7e80db112f4173944c584ada490dcba6c4
Joakim Unge <ashba@unge.se>
2018-03-06 22:48:25 +0100

    Add TokenActions

---
+++ src/actions/TokenActions.js
@@ -0,0 +1,56 @@
+import {
+	TOKEN_REQUEST,
+	TOKEN_SUCCESS,
+	TOKEN_FAILURE
+} from '../constants';
+
+export const requestToken = (token) => {
+	return {
+		type: TOKEN_REQUEST,
+		isFetching: true,
+		isAuthenticated: false,
+		token: token
+	}
+}
+
+export const tokenSuccess = (user) => {
+	return {
+		type: TOKEN_SUCCESS,
+		isFetching: false,
+		isAuthenticated: true,
+		user: user
+	}
+}
+
+export const tokenFailure = (message) => {
+	return {
+		type: TOKEN_FAILURE,
+		isFetching: false,
+		isAuthenticated: false,
+		message
+	}
+}
+
+export const verifyToken = (token) => dispatch => {
+	const options = {
+		method: 'GET',
+		headers: { 
+			'Content-Type': 'application/json',
+			'x-access-token': token
+		}
+	}
+
+	dispatch(requestToken(token));
+
+	return fetch('/auth/verify', options)
+		.then(res => res.json())
+		.then(res => {
+			console.log(res);
+			if (!res._id) {
+				dispatch(tokenFailure('Could not verify token. Please sign in again.'));
+				return Promise.reject(res)
+			}
+			dispatch(tokenSuccess(res))
+		})
+		.catch(err => console.log("Error: ", err));
+}
