bc1f23a07bc9c4c83f07b3a47034ec84741cc027
Joakim Unge <ashba@unge.se>
2018-03-06 23:22:43 +0100

    Add icon attribution to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
 	return (
 		<footer className="App-footer">
 			<h5>by <a href="http://github.com/joakimunge">Joakim Unge</a></h5>
+			<p className="App-footer__attribution">Icons made by <a href="http://www.freepik.com" title="Freepik"><strong>Freepik</strong></a> from <a href="https://www.flaticon.com/" title="Flaticon"><strong>www.flaticon.com</strong></a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"><strong>CC 3.0 BY</strong></a></p>
 		</footer>
 	)
 }
