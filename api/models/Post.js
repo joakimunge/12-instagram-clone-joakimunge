4b0e35e469f72545e2548d68099fc4da4fbc91db
Joakim Unge <ashba@unge.se>
2018-03-07 16:23:16 +0100

    Add description to Postmodel

--- api/models/Post.js
+++ api/models/Post.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
 	image: {type: String, minlength: [1, "An image url can't be empty"]},
+	description: {type: String, minlength: [1, "Description can't be empty"]},
 	author: {type: String},
 	likes: {type: Array},
 	comments: {type: Array}
