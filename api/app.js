47abec96748239154e280694da56569230a62223
Joakim Unge <ashba@unge.se>
2018-03-07 16:24:12 +0100

    use App, Post and LikeController in app

--- api/app.js
+++ api/app.js
@@ -5,9 +5,15 @@ var app = express();
 
 var UserController = require('./controllers/UserController');
 var AuthController = require('./controllers/AuthController');
+var PostController = require('./controllers/PostController');
+var LikeController = require('./controllers/LikeController');
+var CommentController = require('./controllers/CommentController');
 
 app.use(morgan('dev'));
 app.use('/users', UserController);
+app.use('/comments', CommentController);
+app.use('/likes', LikeController);
 app.use('/auth', AuthController);
+app.use('/posts', PostController);
 
 module.exports = app;
