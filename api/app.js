ad6f4e6edcef90192fe5ca0f6cb55c6efe568bb9
Joakim Unge <ashba@unge.se>
2018-03-08 14:36:49 +0100

    Use seeder in app

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var AuthController = require('./controllers/AuthController');
 var PostController = require('./controllers/PostController');
 var LikeController = require('./controllers/LikeController');
 var CommentController = require('./controllers/CommentController');
+var Seeder = require('./controllers/Seeder');
 
 app.use(morgan('dev'));
 app.use('/users', UserController);
@@ -15,5 +16,6 @@ app.use('/comments', CommentController);
 app.use('/likes', LikeController);
 app.use('/auth', AuthController);
 app.use('/posts', PostController);
+app.use('/seed', Seeder);
 
 module.exports = app;
