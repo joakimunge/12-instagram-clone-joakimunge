37c989886b650289394458df85c0ca45f4ed23cf
Joakim Unge <ashba@unge.se>
2018-03-08 22:04:39 +0100

    Implement comment form on feeditem

--- src/components/FeedItem/FeedItem.js
+++ src/components/FeedItem/FeedItem.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Avatar } from '../'
 import './FeedItem.css';
 
+import {
+  createComment
+} from '../../actions';
+
 class FeedItem extends Component {
+
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+        body: ''
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let comment = {
+      body: this.state.body, 
+      postId: this.props.photo._id
+    }
+
+    this.props.dispatch(createComment(comment));
+  }
+
+  handleChange(e) {
+    this.setState({
+      body: e.target.value
+    });
+  }
+
+  componentWillUpdate() {
+    if (this.props.isAuthenticated) {
+      return true;
+    }
+  }
 
   render() {
     const {photo} = this.props;
+    console.log(photo)
     return (
       <article className="FeedItem">
       	<div className="FeedItem__user">
@@ -35,7 +73,9 @@ class FeedItem extends Component {
       			</div>
       		</div>
       		<div className="FeedItem__addcomment">
-      			<input type="text" name="comment" placeholder="Say something nice!"/>
+            <form onSubmit={this.handleSubmit}>
+              <input id="body" type="text" value={this.state.body} onChange={this.handleChange} name="body" placeholder="Say something nice!"/>
+            </form>
       		</div>
       	</div>
       </article>
@@ -43,4 +83,8 @@ class FeedItem extends Component {
   }
 }
 
-export default FeedItem;
+const mapStateToProps = state => ({
+
+})
+
+export default connect(mapStateToProps)(FeedItem);
