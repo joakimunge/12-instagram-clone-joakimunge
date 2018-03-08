ef0dbe6df16447b294622dd64cd200005983796f
Joakim Unge <ashba@unge.se>
2018-03-08 14:37:30 +0100

    Add all-route to PostController

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -4,9 +4,18 @@ var bodyParser = require('body-parser');
 var Post = require ('../models/Post');
 var VerifyToken = require('../middleware/VerifyToken');
 
-router.use(bodyParser.urlencoded({
-	extended: true
-}));
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+router.get('/all', function(req, res) {
+	Post.find({}, function(error, posts) {
+		if (error) {
+			return res.status(500).send("An error occurred while getting all users. Status code 500: Internal server error")
+		} else {
+			return res.status(200).send(posts);	
+		}
+	})
+})
 
 router.post('/', VerifyToken, function(req, res) {
 	Post.create({
@@ -38,6 +47,8 @@ router.get('/:id', VerifyToken ,function(req, res) {
 })
 
 router.delete('/:id', VerifyToken, function(req, res) {
+	// Check if userId is same as Post.Author. Then delete
+	//Otherwise return Unauthorized
 	Post.findByIdAndRemove(req.params.id, function(error, post) {
 		if (error) {
 			return res.status(500).send("An error occurred while trying to remove post. Status code 500: Internal server error")
@@ -48,6 +59,9 @@ router.delete('/:id', VerifyToken, function(req, res) {
 })
 
 router.put('/:id', VerifyToken, function(req, res) {
+	// Check if userId is same as Post.Author. Then update
+	//Otherwise return Unauthorized
+	//For likes and comments this should not apply
 	Post.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(error, post) {
 		if (error) {
 			return res.status(500).send("An error occurred while trying to update post. Status code 500: Internal server error")
