5f43742ba5efaef138be906630719d5f06998a6b
Joakim Unge <ashba@unge.se>
2018-03-08 14:38:20 +0100

    Render posts into correct row in PhotoGrid

--- src/containers/PhotoGrid.js
+++ src/containers/PhotoGrid.js
@@ -15,37 +15,51 @@ class PhotoGrid extends Component {
 		this.props.dispatch(fetchPhotos());
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps !== this.props) {
+			return true;
+		}
+	}
+
 	render() {
-		const {photos} = this.props;
+		let {photos} = this.props;
+		let row = [];
+		let photoRows = [];
+
+		if (photos.length > 0) {
+			photos.map((photo, i)=> {
+				if (i % 3 === 0 || i === 0) {
+					row = [];
+					photoRows.push(row);
+				}
+				row.push(photo)
+			})
+		}
+
+		if (this.props.isFetching) {
+    	return <Loader />;
+		}
+
 		return(
-					this.props.isFetching
-					?
-					<section className="App-PhotoGrid">
-						<div style={styles} className="App-PhotoGrid__row">
-							<GridItem />
-							<GridItem />
-							<GridItem />
-						</div>
-						<div style={styles} className="App-PhotoGrid__row">
-							<GridItem />
-							<GridItem />
-							<GridItem />
-						</div>
-						<div style={styles} className="App-PhotoGrid__row">
-							<GridItem />
-							<GridItem />
-							<GridItem />
-						</div>
-					</section>
-					:
-					<Loader />
+			<section className="App-PhotoGrid">
+			{
+				photoRows.map((row, i) => {
+					return(
+					<div key={i} style={styles} className="App-PhotoGrid__row">
+						{ row.map((photo) => <GridItem key={photo.id} {...photo} />) }
+					</div>
+					)
+				})
+			}
+			</section>
 		)
 	}
 
 }
 
 const mapStateToProps = state => ({
-	photos: state.photos
+	photos: state.root.photos,
+	isFetching: state.root.isFetching
 })
 
 export default connect(mapStateToProps)(PhotoGrid);
