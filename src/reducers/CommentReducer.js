07b37f0376948f89356e11e6230574f627a5a59a
Joakim Unge <ashba@unge.se>
2018-03-08 22:04:09 +0100

    Implement comment reducer

---
+++ src/reducers/CommentReducer.js
@@ -0,0 +1,38 @@
+import {
+	CREATE_COMMENT_REQUEST,
+	CREATE_COMMENT_SUCCESS,
+	CREATE_COMMENT_FAILURE
+} from '../constants';
+
+const initialState = {
+	isAuthenticated: localStorage.getItem('token') ? true : false
+}
+
+export const commentReducer = (state = initialState, action) => {
+	switch(action.type) {
+		case CREATE_COMMENT_REQUEST:
+			return Object.assign({}, state, {
+				payload: action.payload
+			})
+
+		case CREATE_COMMENT_SUCCESS:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: true,
+				user: action.creds,
+				message: 'Authentication successful!'
+			})
+
+		case CREATE_COMMENT_FAILURE:
+			return Object.assign({}, state, {
+				isFetching: false,
+				isAuthenticated: false,
+				message: action.message
+			})
+			
+		default:
+			return state;
+	}
+}
+
+export default authReducer;
