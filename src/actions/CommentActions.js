ce19cd6c889132cc236651d9234711a611e589e5
Joakim Unge <ashba@unge.se>
2018-03-08 22:04:00 +0100

    Add comment actions

---
+++ src/actions/CommentActions.js
@@ -0,0 +1,47 @@
+import {
+	CREATE_COMMENT_REQUEST,
+	CREATE_COMMENT_SUCCESS,
+	CREATE_COMMENT_FAILURE
+} from '../constants';
+
+const commentsUrl = `/comments/`;
+
+export const requestComment = (comment) => ({
+	type: CREATE_COMMENT_REQUEST,
+	payload: comment
+});
+
+export const commentSuccess = (comment) => ({
+	type: CREATE_COMMENT_SUCCESS,
+	payload: comment
+});
+
+export const commentFailure = () => ({
+	type: CREATE_COMMENT_FAILURE
+});
+
+export const createComment = (comment) => dispatch => {
+	dispatch(requestComment());
+
+	console.log(comment);
+
+	const options = {
+		method: 'POST',
+		headers: { 
+			'Content-Type': 'application/json',
+			'x-access-token': localStorage.getItem('access_token') || null
+		},
+		body: JSON.stringify(comment)
+	}
+
+	return fetch(commentsUrl, options)
+		.then(res => res.json())
+		.then(comment => {
+			console.log('Successfully posted comment: ', comment);
+			dispatch(commentSuccess(comment));
+		})
+		.catch(error => {
+			console.error('An error occurred: ', error);
+			dispatch(commentFailure())
+		})
+}
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 export { fetchPhotos } from './PhotoActions.js';
+export { createComment } from './CommentActions.js';
 export { loginUser, logoutUser } from './AuthActions.js';
 export { verifyToken, tokenFailure } from './TokenActions.js';
