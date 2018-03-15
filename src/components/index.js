e503356f380f35f3a385c69f9f67edc51ddc7b52
Joakim Unge <ashba@unge.se>
2018-03-15 22:10:46 +0100

    export button comp

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as App } from './App/App.js';
 export { default as Header } from './Header/Header.js';
 export { default as Footer } from './Footer/Footer.js';
+export { default as Button } from './Button/Button.js';
 export { default as ErrorMessage } from './ErrorMessage/ErrorMessage.js';
 export { default as FeedItem } from './FeedItem/FeedItem.js';
 export { default as GridItem } from './GridItem/GridItem.js';
