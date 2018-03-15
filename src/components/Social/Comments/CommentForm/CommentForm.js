4b3ecf39e67e849a8534eddfda7fe6bf18768bbc
Joakim Unge <ashba@unge.se>
2018-03-15 14:23:38 +0100

    Add commentform component

---
+++ src/components/Social/Comments/CommentForm/CommentForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Avatar, Like, Comment } from '../../../';
+
+import {
+  createComment,
+  fetchPhotoInfo
+} from '../../../../actions';
+
+class CommentForm extends Component {
+
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+        body: ''
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let comment = {
+      body: this.state.body, 
+      postId: this.props._id
+    }
+
+    this.props.dispatch(createComment(comment));
+    this.setState({
+      body: ''
+    })
+  }
+
+  handleChange(e) {
+    this.setState({
+      body: e.target.value
+    });
+  }
+
+  componentWillUpdate() {
+    if (this.props.isAuthenticated) {
+      return true;
+    }
+  }
+
+  render() {
+    return (
+  		<div className="FeedItem__addcomment">
+        <form onSubmit={this.handleSubmit}>
+          <input id="body" type="text" value={this.state.body} onChange={this.handleChange} name="body" placeholder="Say something nice!"/>
+        </form>
+  		</div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isSubmitting: false
+})
+
+export default connect(mapStateToProps)(CommentForm);
