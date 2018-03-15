f29e03e63a93314d794cc2bab398f8d9468165ff
Joakim Unge <ashba@unge.se>
2018-03-15 14:22:59 +0100

    Add actions for photo info fetching

--- src/actions/PhotoActions.js
+++ src/actions/PhotoActions.js
@@ -1,12 +1,13 @@
 import {
 	FETCH_PHOTOS_START,
 	FETCH_PHOTOS_SUCCESS,
-	FETCH_PHOTOS_FAILURE
+	FETCH_PHOTOS_FAILURE,
+	FETCH_PHOTO_INFO_START,
+	FETCH_PHOTO_INFO_SUCCESS,
+	FETCH_PHOTO_INFO_FAILURE
 } from '../constants';
 
-const mockapi = `http://5a99458d5217dd0012c78947.mockapi.io/photos`;
-const postsUrl = `/posts/all`;
-
+//All Photos
 export const requestPhotos = () => ({
 	type: FETCH_PHOTOS_START
 });
@@ -22,7 +23,7 @@ export const rejectedPhotos = () => ({
 
 export const fetchPhotos = () => dispatch => {
 	dispatch(requestPhotos());
-	return fetch(postsUrl)
+	return fetch('/posts/all')
 		.then(res => res.json())
 		.then(data => {
 			console.log('Successfully got data: ', data);
@@ -31,5 +32,33 @@ export const fetchPhotos = () => dispatch => {
 		.catch(error => {
 			console.error('An error occurred: ', error);
 			dispatch(rejectedPhotos())
+		})
+}
+
+// Single photo
+export const requestPhotoInfo = () => ({
+	type: FETCH_PHOTO_INFO_START
+});
+
+export const receivePhotoInfo = (photo) => ({
+	type: FETCH_PHOTO_INFO_SUCCESS,
+	payload: photo
+});
+
+export const rejectedPhotoInfo = () => ({
+	type: FETCH_PHOTO_INFO_FAILURE
+});
+
+export const fetchPhotoInfo = (id) => dispatch => {
+	dispatch(requestPhotoInfo());
+	return fetch('/posts/' + id)
+		.then(res => res.json())
+		.then(data => {
+			console.log('Got photo info: ', data);
+			dispatch(receivePhotoInfo(data))
+		})
+		.catch(error => {
+			console.error('An error occurred: ', error);
+			dispatch(rejectedPhotoInfo());
 		})
 }
