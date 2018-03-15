7cdd87aa5ec06a4aea2db1411fd41c15efecddaf
Joakim Unge <ashba@unge.se>
2018-03-15 14:22:08 +0100

    Implement ability to toggle like on and off

--- api/controllers/LikeController.js
+++ api/controllers/LikeController.js
@@ -1,88 +1,55 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Like = require ('../models/Like');
-var Post = require ('../models/Post');
+var Like = require('../models/Like');
+var Post = require('../models/Post');
+var User = require('../models/User');
 var VerifyToken = require('../middleware/VerifyToken');
 
 router.use(bodyParser.urlencoded({
-	extended: true
+  extended: false
 }));
 router.use(bodyParser.json());
 
 router.post('/', VerifyToken, function(req, res) {
-	Post.findAndModify({
-		query: {_id: req.postId},
-		$addToSet: {'likes': {$elemMatch: {author: req.userUd}}},
-		function(error, post) {
-			if(error) {
-				console.log(error);
-				return;
-			}
-			console.log(post)
-			return;
-		}
-	})
+  Post.findByIdAndUpdate(req.body.postId, req.userId, function(error, post) {
+    if (error) return res.status(500).send({ auth: true, error: error.message });
 
-	return console.log(done);
-	
-	Post.findOne({'likes': {$elemMatch: {author: req.userId}}}, function(error, post) {
-		if (error) {
-			console.log(error);
-			return;
-		}
-		console.log(post);
-		return;
-	});
+    var userHasLiked = post.likes.indexOf(req.userId)
 
-	
-	Like.create({
-			author: req.userId
-		}, function(error, like) {
-			if (error) {
-				return res.status(500).send("An error occurred while trying to add like. Status code 500: Internal server error")
-			} 
-			else {
-				Post.findByIdAndUpdate(req.body.postId, { $push: { likes: like }}, function(error, like) {
-					if (error) {
-							return res.status(500).send("An error occurred while trying to add like. Status code 500: Internal server error")
-						} 
-					
-					return res.status(200).send({auth: true, like: like});							
-				})
-			}
-		}
-	);
+    if (userHasLiked === -1) {
+      post.likes.push(req.userId)
+    } else {
+      post.likes.splice(userHasLiked, 1)
+    }
+
+    post.save();
+  }).then(function(post) {
+    User.findById(req.userId, function(error, user) {
+      if (error) return res.status(500).send({ auth: true, error: error.message });
+
+      var userHasLiked = user.likes.indexOf(req.body.postId)
+
+      if (userHasLiked === -1) {
+        user.likes.push(req.body.postId)
+
+      } else {
+        user.likes.splice(userHasLiked, 1)
+      }
+
+      user.save();
+      return res.status(200).send({ auth: true, post: post, user: user });
+    })
+  })
 })
 
 router.get('/:id', function(req, res) {
-	Like.findById(req.params.id, function(error, like) {
-		if (error) {
-			return res.status(500).send("An error occurred while getting like info. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send(like);	
-		}
-	})
-})
-
-router.delete('/:id', function(req, res) {
-	Like.findByIdAndRemove(req.params.id, function(error, like) {
-		if (error) {
-			return res.status(500).send("An error occurred while trying to remove like. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send("like " + like.name + " was successfully deleted.");	
-		}
-	})
-})
-
-router.put('/:id', function(req, res) {
-	Like.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(error, like) {
-		if (error) {
-			return res.status(500).send("An error occurred while trying to update like. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send("like " + like.name + "was successfully updated. New like info: ", like);	
-		}
-	})
+  Like.findById(req.params.id, function(error, like) {
+    if (error) {
+      return res.status(500).send("An error occurred while getting like info. Status code 500: Internal server error")
+    }
+    return res.status(200).send(like);
+  })
 })
 
 module.exports = router;
