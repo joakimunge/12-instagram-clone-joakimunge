f7374e614763de27bd92770492d349517e8f6e56
Joakim Unge <ashba@unge.se>
2018-03-15 14:23:16 +0100

    Move comments to own component

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './Comment.css';
+
+class Comment extends Component {
+	 constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+  	let comment = {
+  		commentId: this.props._id
+  	}
+  	// this.props.dispatch(deleteComment(comment))
+  }
+
+	render() {
+    const comment = this.props;
+    return <div className="FeedItem__comment">
+            <span className="FeedItem__username">{comment.author}</span>
+            <p className="FeedItem__comment__body">{comment.body}</p>
+            <span onClick={this.handleClick} className="FeedItem__comment__delete"><i className="fas fa-times"></i></span>
+          </div>
+	}
+}
+
+const mapStateToProps = state => ({
+})
+
+export default connect(mapStateToProps)(Comment);
