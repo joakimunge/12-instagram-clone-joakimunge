40fcca237812e14b550610f07e20788f6bb56442
Joakim Unge <ashba@unge.se>
2018-03-15 22:06:57 +0100

    Update validation on usermodel

--- api/models/User.js
+++ api/models/User.js
@@ -1,15 +1,20 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
-	username: {type: String, minlength: [3, "Username must be at least 3 characters"]},
-	name: {type: String, maxlength: [100, "Too long name"]},
-	email: String,
-	password: {type: String, minlength: [8, "Too short password"]},
+	username: {type: String, required: true, unique: true, minlength: [3, "Username must be at least 3 characters"]},
+	name: {type: String, unique: true, required: true, maxlength: [100, "Too long name"]},
+	email: {type: String, unique: true, required: true},
+	password: {type: String, required: true, minlength: [8, "Too short password"]},
+	avatar: {type: String, default: 'https://api.adorable.io/avatars/285/4@4.png'},
+	description: {type: String, default: "is on Aperture! But.. why hasn't he written a description? :/"},
 	posts: {type: Array},
 	likes: {type: Array},
 	comments: {type: Array}
 });
+
+UserSchema.plugin(uniqueValidator);
 
 UserSchema.pre('save', function(next) {
 	var user = this;
