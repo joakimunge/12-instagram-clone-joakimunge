9ad15191cc772fc157e05819aa47de96702917d5
Joakim Unge <ashba@unge.se>
2018-03-15 14:22:44 +0100

    Clean up commentcontroller

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -1,71 +1,70 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Comment = require ('../models/Comment');
-var Post = require ('../models/Post');
+var Comment = require('../models/Comment');
+var Post = require('../models/Post');
+var User = require('../models/User');
 var VerifyToken = require('../middleware/VerifyToken');
 
 router.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 router.use(bodyParser.json());
 
 router.post('/', VerifyToken, function(req, res) {
 
-	Comment.create({
-			body: req.body.body,
-			author: req.userId
-		}, function(error, comment) {
+  User.findById(req.userId, function(error, user) {
+    if (error) return res.status(500).send({auth: true, error: error.message});
+  }).then(function(user) {
+    Comment.create({
+      body: req.body.body,
+      author: user.username,
+      author_id: user._id
+    }, function(error, comment) {
 
-			if (error) {
-				return res.status(500).send("An error occurred while trying to add comment. Status code 500: Internal server error")
-			} 
+      if (error) return res.status(500).send({auth: true, error: error.message})
 
-			else {
-				Post.findByIdAndUpdate(req.body.postId, { $push: { comments: comment._id }}, function(error, comment) {
-					if (error) {
-							return res.status(500).send("An error occurred while trying to add comment. Status code 500: Internal server error")
-						} 
-					
-					return res.status(200).send({auth: true, comment: comment});							
-				})
-			}
-		});
+      Post.findByIdAndUpdate(req.body.postId, { $push: { comments: comment } }, function(error, post) {
+        if (error) return res.status(500).send({auth: true, error: error.message})
+        
+        return res.status(200).send({ auth: true, post: post });
+      })
+    });
+  })
 })
 
 router.get('/:id', function(req, res) {
-	Comment.findById(req.params.id, function(error, comment) {
-		if (error) {
-			return res.status(500).send("An error occurred while getting comment info. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send(comment);	
-		}
-	})
+  Comment.findById(req.params.id, function(error, comment) {
+    if (error) {
+      return res.status(500).send("An error occurred while getting comment info. Status code 500: Internal server error")
+    } else {
+      return res.status(200).send(comment);
+    }
+  })
 })
 
 router.delete('/:id', function(req, res) {
-	Comment.findByIdAndRemove(req.params.id, function(error, comment) {
-		if (error) {
-			return res.status(500).send("An error occurred while trying to remove comment. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send("Comment " + comment.name + " was successfully deleted.");	
-		}
-	})
+  Comment.findByIdAndRemove(req.params.id, function(error, comment) {
+    if (error) {
+      return res.status(500).send("An error occurred while trying to remove comment. Status code 500: Internal server error")
+    } else {
+      return res.status(200).send("Comment " + comment.name + " was successfully deleted.");
+    }
+  })
 })
 
 router.put('/:id', function(req, res) {
-	Comment.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(error, comment) {
-		if (error) {
-			return res.status(500).send("An error occurred while trying to update comment. Status code 500: Internal server error")
-		} else {
-			return res.status(200).send("Comment " + comment.name + "was successfully updated. New comment info: ", comment);	
-		}
-	})
+  Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(error, comment) {
+    if (error) {
+      return res.status(500).send("An error occurred while trying to update comment. Status code 500: Internal server error")
+    } else {
+      return res.status(200).send("Comment " + comment.name + "was successfully updated. New comment info: ", comment);
+    }
+  })
 })
 
 router.get('/all', VerifyToken, function(req, res) {
-	Comment.find()
+  Comment.find()
 })
 
 module.exports = router;
-
