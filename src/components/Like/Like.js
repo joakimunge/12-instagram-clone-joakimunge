ca7137cc943237707ac9744cb11ce5dc0348db13
Joakim Unge <ashba@unge.se>
2018-03-16 17:30:45 +0100

    Toggle liked/notliked on click

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -14,18 +14,21 @@ class Like extends Component {
 
   handleClick(e) {
   	let like = {
-  		postId: this.props._id
+  		postId: this.props._id,
+      userId: this.props.user._id
   	}
   	this.props.dispatch(createLike(like))
   }
 
 	render() {
-		return <li onClick={this.handleClick} className="App-Like"><i className="far fa-heart"></i></li>;
+    const {toggle} = this.props;
+    if (toggle) return <li onClick={this.handleClick} className="App-Like"><i className="far fa-heart"></i></li>;
+		return <li onClick={this.handleClick} className="App-Like liked"><i className="fas fa-heart"></i></li>;
 	}
 }
 
 const mapStateToProps = state => ({
-	toggle: state.like.toggle
+  user: state.auth.user
 })
 
 export default connect(mapStateToProps)(Like);
