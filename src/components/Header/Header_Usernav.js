4901f83d8cd6a7b781b14ce204671d23f4ee9a19
Joakim Unge <ashba@unge.se>
2018-03-16 10:45:49 +0100

    Add upload icon and link to header

--- src/components/Header/Header_Usernav.js
+++ src/components/Header/Header_Usernav.js
@@ -4,6 +4,7 @@ import logo from '../../aperture.svg';
 import explore from '../../binoculars.svg';
 import userIcon from '../../user.svg';
 import notification from '../../notification.svg';
+import photo from '../../photo.svg';
 
 class Usernav extends Component {
 
@@ -15,6 +16,7 @@ class Usernav extends Component {
 		return (
 			<div className="App-header__navigation">
 				<ul>
+					<li><Link to="/upload"><img src={photo} className="App-explore" alt="upload" width="36px"/></Link></li>
 					<li><Link to="/explore"><img src={explore} className="App-explore" alt="explore" width="36px"/></Link></li>
 					<li><img src={notification} className="App-notification" alt="notifications" width="36px"/></li>
 					<li><Link to={`/users/${this.props.username}`}><img src={userIcon} className="App-user" alt="my profile" width="36px"/></Link></li>
