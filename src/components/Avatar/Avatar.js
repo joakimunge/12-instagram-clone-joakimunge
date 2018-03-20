3fac0670d2f8e7376ea5045035e18e5128c5b827
Joakim Unge <ashba@unge.se>
2018-03-20 10:22:43 +0100

    Update avatar component to be dynamic

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Avatar.css';
 
-const Avatar = () => {
+const Avatar = (props) => {
 	return (
 		<span className="Avatar Avatar--small">
-			<img src="https://avatars1.githubusercontent.com/u/24225542?s=460&v=4" alt="avatar avatar--small" width="36px" />
+			<img src={props.url} alt="avatar avatar--small" width="36px" />
 		</span>
 	)
 }
