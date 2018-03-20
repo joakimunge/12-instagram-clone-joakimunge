79fe49af1000d55738188f8423f835b2130d33c8
Joakim Unge <ashba@unge.se>
2018-03-20 08:56:15 +0100

    Move upload form request to PostActions

--- src/containers/UploadForm.js
+++ src/containers/UploadForm.js
@@ -6,31 +6,23 @@ import { Loader, Button } from '../components';
 
 import logo from '../aperture.svg';
 
-class SignUpForm extends Component {
+import {
+	createPost
+} from '../actions';
+
+class UploadForm extends Component {
 
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
-		this.state = {email: '', password: ''}
+		this.state = {}
 	}
 
 	handleSubmit(e) {
 		e.preventDefault();
-		const headers = new Headers();
-		headers.append('Content-Type', 'application/json');
-
-		const options = {
-			method: 'POST',
-			headers,
-			body: JSON.stringify(this.state)
-		};
-
-		const request = new Request('/auth/register', options);
-
-		fetch(request)
-			.then(res => res.json())
-			.then(res => console.log(res));
+		const formData = new FormData(e.target);
+		this.props.dispatch(createPost(formData));
 	}
 
 	handleChange(e) {
@@ -42,12 +34,22 @@ class SignUpForm extends Component {
 	}
 
 	render() {
+		const {success, isSubmitting} = this.props;
+
+		if (isSubmitting) {
+			return <Loader />
+		}
+
+		if (success) {
+			return <Redirect to="/" />
+		}
+
 		return(
 			<section className="App-signup form-wrapper">
 				<h2 className="App-signup">Upload new post!</h2>
-				<form className="form-control" onSubmit={this.handleSubmit} enctype="multipart/form-data">
-					<input id="image" type="file" placeholder="Image" accept="image/*" />
-					<input id="description" type="text" value={this.state.name} onChange={this.handleChange} placeholder="Say something about your image.." />
+				<form className="form-control" onSubmit={this.handleSubmit} encType="multipart/form-data">
+					<input name="mediapost" id="image" type="file" placeholder="Image" accept="image/*" required />
+					<textarea name="description" id="description" type="text" value={this.state.name} onChange={this.handleChange} placeholder="Say something about your image.." style={{resize: 'none', height: '140px'}}/>
 					<Button style="primary" text="Submit" type="submit" />
 				</form>
 			</section>
@@ -55,4 +57,9 @@ class SignUpForm extends Component {
 	}
 }
 
-export default SignUpForm;
+const mapStateToProps = state => ({
+	isSubmitting: state.posts.isSubmitting,
+	success: state.posts.success
+})
+
+export default connect(mapStateToProps)(UploadForm);
