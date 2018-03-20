b1b1ffb2ba4c5aa437cebdbdb716d7c6c89482a2
Joakim Unge <ashba@unge.se>
2018-03-20 08:55:23 +0100

    Add CRATE_POST actions

--- src/actions/PostActions.js
+++ src/actions/PostActions.js
@@ -4,8 +4,16 @@ import {
 	FETCH_POSTS_FAILURE,
 	FETCH_POST_START,
 	FETCH_POST_SUCCESS,
-	FETCH_POST_FAILURE
+	FETCH_POST_FAILURE,
+	CREATE_POST_REQUEST,
+	CREATE_POST_SUCCESS,
+	CREATE_POST_FAILURE,
+
 } from '../constants';
+
+import {
+	getToken
+} from '../middleware/GetToken'
 
 //All posts
 export const requestPosts = () => ({
@@ -17,20 +25,34 @@ export const receivePosts = (posts) => ({
 	payload: posts
 });
 
-export const rejectedPosts = () => ({
-	type: FETCH_POSTS_FAILURE
+export const rejectedPosts = (message) => ({
+	type: FETCH_POSTS_FAILURE,
+	message: message
 });
 
 export const fetchPosts = () => dispatch => {
 	dispatch(requestPosts());
-	return fetch('/posts/all')
+
+	const token = getToken()
+
+	if (!token) {
+		return dispatch(rejectedPosts('Could not load token'))
+	}
+
+	const options = {
+		method: 'GET',
+		headers: { 
+			'Content-Type': 'application/json',
+			'x-access-token': token
+		}
+	}
+
+	return fetch('/posts/all', options)
 		.then(res => res.json())
 		.then(data => {
-			console.log('Successfully got data: ', data);
 			dispatch(receivePosts(data));
 		})
 		.catch(error => {
-			console.error('An error occurred: ', error);
 			dispatch(rejectedPosts())
 		})
 }
@@ -54,11 +76,55 @@ export const fetchPostInfo = (id) => dispatch => {
 	return fetch('/posts/' + id)
 		.then(res => res.json())
 		.then(data => {
-			console.log('Got post info: ', data);
 			dispatch(receivePostInfo(data))
 		})
 		.catch(error => {
-			console.error('An error occurred: ', error);
 			dispatch(rejectedPostInfo());
+		})
+}
+
+
+//Create post
+export const requestCreatePost = (formData) => ({
+	type: CREATE_POST_REQUEST,
+	payload: formData
+});
+
+export const receiveCreatePost = (post) => ({
+	type: CREATE_POST_SUCCESS,
+	payload: post
+});
+
+export const rejectedCreatePost = (message) => ({
+	type: CREATE_POST_FAILURE,
+	message: message
+});
+
+export const createPost = (formData) => dispatch => {
+	const token = getToken();
+
+	if (!token) {
+		return dispatch(rejectedCreatePost('Could not load token'))
+	}
+
+	dispatch(requestCreatePost(formData));
+	const headers = new Headers();
+	headers.append('x-access-token', token);
+
+	const options = {
+		method: 'POST',
+		headers,
+		body: formData
+	};
+
+	const request = new Request('/posts', options);
+
+	return fetch(request)
+		.then(res => res.json())
+		.then(post => {
+			dispatch(receiveCreatePost(post))
+		})
+		.catch(error => {
+			dispatch(rejectedCreatePost(error));
 		})
 }
