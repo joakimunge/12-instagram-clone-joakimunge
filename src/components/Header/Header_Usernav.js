b61d842f571d0771c64a2bdec5037be584d5e019
Joakim Unge <ashba@unge.se>
2018-03-20 08:57:45 +0100

    Toggle dropdown on click in usernav

--- src/components/Header/Header_Usernav.js
+++ src/components/Header/Header_Usernav.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Dropdown } from '../';
+
 import logo from '../../aperture.svg';
 import explore from '../../binoculars.svg';
 import userIcon from '../../user.svg';
@@ -10,20 +13,36 @@ class Usernav extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = { isOpen: true }
 	}
+
+	toggleDropdown = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
 	render() {
 		return (
 			<div className="App-header__navigation">
-				<ul>
+				<ul className="App-header__navigation__list">
 					<li><Link to="/upload"><img src={photo} className="App-explore" alt="upload" width="36px"/></Link></li>
 					<li><Link to="/explore"><img src={explore} className="App-explore" alt="explore" width="36px"/></Link></li>
 					<li><img src={notification} className="App-notification" alt="notifications" width="36px"/></li>
 					<li><Link to={`/users/${this.props.username}`}><img src={userIcon} className="App-user" alt="my profile" width="36px"/></Link></li>
+					<li onClick={this.toggleDropdown} ><img src={userIcon} className="App-user" alt="my profile" width="36px"/>
+						<Dropdown {...this.props} show={this.state.isOpen}
+		          onClose={this.toggleModal}>
+		        </Dropdown>
+					</li>
 				</ul>
 			</div>
 		)
 	}
 }
 
-export default Usernav;
+const mapStateToProps = state => ({
+	user: state.auth.user
+})
+
+export default connect(mapStateToProps)(Usernav);
