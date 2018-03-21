69fa2138de093c10617ce228c54866fe00e2a219
Joakim Unge <ashba@unge.se>
2018-03-21 15:14:42 +0100

    require cloudinary

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 var app = require('./app');
 require('dotenv').config();
+
 var port = process.env.PORT || 3001;
 
-var server = app.listen(port, function() {
+app.listen(port, function() {
 	console.log('Express is now listening to port: ' + port);
 });
 
